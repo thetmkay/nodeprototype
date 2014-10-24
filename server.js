771e8442dd92a486ec42f78cbcdab723bbeaf8d8
George Nishimura <thetmkay@gmail.com>
2014-10-24 03:16:08 +0100

    fix for express 4.0

--- server.js
+++ server.js
@@ -2,41 +2,16 @@
  * Module dependencies
  */
 
-var express = require('express'),
-  http = require('http'),
+var http = require('http'),
   path = require('path');
 
-var app = module.exports = express();
-
-/**
- * Configuration
- */
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-
-
-
-/**
- * Routes
- */
-
-app.get('/:name', function(req,res) {
-  res.sendfile(req.params.name);
-});
-// redirect all others to the index (HTML5 history)
-app.get('*', function(req,res) {
-  res.sendfile('./views/index.html');
-});
-
+var app = require('./app')
 
 /**
  * Start Server
  */
 
-http.createServer(app).listen(app.get('port'), function () {
+app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
